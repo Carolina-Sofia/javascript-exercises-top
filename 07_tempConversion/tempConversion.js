f3add1f220d7018249e1a2e100487dc951815ffc
Carolina B <c.sbernardes02@gmail.com>
2024-11-16 17:40:54 +0000

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let Celsius = Math.round(((num - 32) / 1.8)*10)/10;
+  return Celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let Fahrenheit = Math.round(((num * 1.8) + 32)*10 ) /10;
+  return Fahrenheit;
 };
 
 // Do not edit below this line
