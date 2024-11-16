f3e8647e29c5aeb887fe179dd4a54c875e4c5b61
Carolina B <c.sbernardes02@gmail.com>
2024-11-16 18:31:38 +0000

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (inputValue) {
+
+    function removeAlpha(str) {
+        return str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    }
+    
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+
+    const processedInput = removeAlpha(inputValue);
+    const reversedInput = reverseString(processedInput);
+    
+    if (processedInput === reversedInput) {
+            return true;
+        } else {
+            return false;
+        }
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
