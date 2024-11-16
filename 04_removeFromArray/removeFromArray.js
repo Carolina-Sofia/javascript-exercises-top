40fed22ea88267a760d9079325df473ead9183c6
Carolina B <c.sbernardes02@gmail.com>
2024-11-16 13:36:49 +0000

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (const value of valuesToRemove) {
+        let index;
+        while ((index = array.indexOf(value)) !== -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
+
+module.exports = removeFromArray;
 
 // Do not edit below this line
 module.exports = removeFromArray;
