ee76de4d836ab49e2a8d04c68609bed1403dc87b
Carolina B <c.sbernardes02@gmail.com>
2024-11-16 18:19:47 +0000

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  let sub = a - b;
+  return sub;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((total, num) => total + num, 0);
+};
+
+const multiply = function(...numbers) {
+  numbers = numbers.flat();
+  if (!numbers.every(num => typeof num === 'number')) {
+    throw new Error("All arguments must be numbers");
+  }
+
+  return numbers.reduce((acc, num) => acc * num, 1);
+}
+
+
+
+const power = function(a, b) {
+  let pow = a ** b;
+  return pow;
 	
 };
 
-const multiply = function() {
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
