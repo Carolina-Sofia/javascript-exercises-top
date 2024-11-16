7d3b680a555f1ac25ea2a2ebd92b4c80860c57fd
Carolina B <c.sbernardes02@gmail.com>
2024-11-16 12:44:39 +0000

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let s = '';
+    if (num < 0){
+        return 'ERROR';
+    }
+    else{
+    for (let i = 0; i < num; i++) {
+        s = s.concat(string); // Append `string` to `s` each iteration.
+    }
+    return s;
+}
 
 };
 
